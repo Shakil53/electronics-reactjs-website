bb7bf6b3e6c555249008b77ffa9fbd4a41f956e0
Shakil53 <shakilahmed.dcc@gmail.com>
2022-12-11 01:45:14 +0600

    container ready

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className='product-container'>
+                <h2>This is product </h2>
+            </div>
+            <div className='cart-container'>
+                <h3>Order Summary</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
