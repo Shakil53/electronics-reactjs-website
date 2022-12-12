02ba0978463e151205a5cf94d0503442f7328060
Shakil53 <shakilahmed.dcc@gmail.com>
2022-12-12 12:25:47 +0600

    done with its cart container

--- src/components/Shop/Cart/Cart.js
+++ src/components/Shop/Cart/Cart.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import './Cart.css';
 
-const Cart = () => {
+const Cart = (props) => {
+    const { cart } = props;
+    console.log(cart);
+
+    let total = 0;
+    for (const product of cart) {
+        total = total + parseFloat(product.price);
+
+    }
+
+    const tax = total * 0.1;
+
+    const grandTotal = total + tax;
+
+
     return (
-        <div>
-
+        <div className='cart-container'>
+            <h4 style={{ textAlign: 'center', fontFamily: 'Lato' }}>Order Summary</h4>
+            <p>Selected Item: {cart.length}</p>
+            <p>Total Price: ${total}</p>
+            <p>Tax: {tax}</p>
+            <h5>Grand Total: {grandTotal}</h5>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Cart from './Cart/Cart';
 import Product from './Product/Product';
 import './Shop.css';
 
@@ -14,7 +15,6 @@ const Shop = () => {
     }, [])
 
     const handleAddToCart = (product) => {
-        console.log(product);
         const newCart = [...cart, product];
         setCart(newCart)
     }
@@ -31,8 +31,7 @@ const Shop = () => {
                 }
             </div>
             <div className='cart-container'>
-                <h4>Order Summary</h4>
-                <p>Selected Item: {cart.length}</p>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
